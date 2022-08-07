7760b53418ba44201fd36551d215d4b65806d817
joshua constine <joshua.constine97@gmail.com>
2022-08-06 18:04:42 -0700

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>main page</div>;
+  let [data, setData] = useState({});
+
+  // 3. Create out useEffect function
+  useEffect(() => {
+    const url = "https://api.census.gov/data/2017/ecnbasic.json";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>{}</div>;
 }
 
 export default App;
